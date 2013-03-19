ff153f0c6c35fd2344e8e63d043bd9cabd3786d0
Kryptykfysh <kryptykfysh@kryptykfysh.co.uk>
2013-03-19 22:50:45 +1100

    Added bootstrap.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Omrails::Application.routes.draw do
+  get "about" => 'pages#about'
+  get "home" => 'pages#home'
+
+  root :to => 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
